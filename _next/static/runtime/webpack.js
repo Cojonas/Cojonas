c9cdf885820af3f700339c8efad8b15ecdd12047
Cojonas <strunzi.js@gmail.com>
2020-10-08 12:03:39 +0200

    EZ.-erm. new

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c19345fe8a287e0a8a33";
+/******/ 	var hotCurrentHash = "276ad5bc03c782a0ac33";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
